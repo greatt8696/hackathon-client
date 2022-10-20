49cc61fd800d15e5346e3cdde3be575802272030
greatt8696 <88331836+greatt8696@users.noreply.github.com>
2022-10-20 09:20:16 +0900

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import ScrollToTop from "@/base-components/scroll-to-top/Main";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import store from "./redux/store";
 import Router from "./router";
 
 function App() {
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Router />
-        <ScrollToTop />
-      </BrowserRouter>
-    </RecoilRoot>
+    <Provider store={store}>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Router />
+          <ScrollToTop />
+        </BrowserRouter>
+      </RecoilRoot>
+    </Provider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import App from './App'
-import './assets/css/app.css'
-import store from './redux/store'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./assets/css/app.css";
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-)
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
