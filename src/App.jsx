67bdd77c11705a04371199aaef4530b68aca45e4
greatt8696 <greatt8696@gmail.com>
2022-10-30 19:57:53 +0900

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-
 import ScrollToTop from '@/base-components/scroll-to-top/Main'
+import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
+import { initChart, initCoinsPrice } from './redux/apiActions'
 import Router from './router'
 
 import { getUserSocket, getTradeSocket } from './socket'
 
 function App() {
   const dispatch = useDispatch()
-
-  getUserSocket(dispatch)
-  getTradeSocket(dispatch)
+  
+  useEffect(() => {
+    getUserSocket(dispatch)
+    getTradeSocket(dispatch)
+    initChart(dispatch)
+    initCoinsPrice(dispatch)
+  },[])
 
   return (
-      <RecoilRoot>
-        <BrowserRouter>
-          <Router />
-          <ScrollToTop />
-        </BrowserRouter>
-      </RecoilRoot>
-  );
+    <RecoilRoot>
+      <BrowserRouter>
+        <Router />
+        <ScrollToTop />
+      </BrowserRouter>
+    </RecoilRoot>
+  )
 }
 
 export default App
